3e4ab08e0e72daf5c9f31ce922de51fc4e495cd4
Eduardo Aquino <edutraquino@gmail.com>
2022-06-04 16:04:01 -0300

    feat: using chalk

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
+import chalk from "chalk";
 import app from "./config/express-app";
 
 const PORT = process.env.PORT || 3000;
 
 function logProcessStart() {
   const host = "http://localhost";
-  console.log(`Server is running at ${host}:${PORT} 🚀`);
+  console.log(
+    `Server is running at ${chalk.blue(host)}:${chalk.green(PORT)} 🚀`
+  );
 }
 
 app.listen(PORT, logProcessStart);
