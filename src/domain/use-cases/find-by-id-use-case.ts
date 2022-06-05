1bdffeea756e62d62657684adfaaf75f4f827610
Eduardo Aquino <edutraquino@gmail.com>
2022-06-05 16:21:52 -0300

    feat: make find by id use case function

---
+++ src/domain/use-cases/find-by-id-use-case.ts
@@ -0,0 +1,7 @@
+import IRepository from "../../output/repositories/repository-interface";
+
+function makeFindByIdUseCase<T>(repository: IRepository<T>) {
+  return async (id: string) => repository.findById(id);
+}
+
+export { makeFindByIdUseCase };
