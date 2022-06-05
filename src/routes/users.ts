2ea8907a4031eae66181116898f1af2d6c46d6aa
Eduardo Aquino <edutraquino@gmail.com>
2022-06-05 17:24:31 -0300

    refact: /users/:id respond using controller

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import User from "../domain/entities/user";
+import { makeFindByIdController } from "../input/controllers/find-by-id-controller";
 
-export default (router: Router) => {
-  router.get("/users/:id", (req, res) => res.send(200));
+export default function (router: Router, repositories: any) {
+  const findByIdController = makeFindByIdController<User.Entity>(
+    repositories.users
+  );
+
+  router.get("/users/:id", findByIdController);
   // router.post("/users", createUserController)
   // router.put("/users/:id", updateUserController)
   // router.delete("/users/:id", deleteUserController)
-};
+}
