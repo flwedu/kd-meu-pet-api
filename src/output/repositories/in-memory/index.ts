4c5266335aae59aa49cf1609111fb0251a86723f
Eduardo Aquino <edutraquino@gmail.com>
2022-06-06 18:59:23 -0300

    refact: rename file and upt occurrences references

--- src/output/repositories/in-memory/index.ts
+++ src/output/repositories/in-memory/index.ts
@@ -1,11 +1,11 @@
 import InMemoryBaseRepository from "./in-memory-base-repository";
 import UsersRepositoryInMemory from "./users-repository-in-memory";
 import AnimalsRepositoryInMemory from "./animals-repository-in-memory";
-import OccurencesRepositoryInMemory from "./occurences-repository-in-memory";
+import OccurrencesRepositoryInMemory from "./occurrences-repository-in-memory";
 
 export {
   InMemoryBaseRepository,
   UsersRepositoryInMemory,
   AnimalsRepositoryInMemory,
-  OccurencesRepositoryInMemory,
+  OccurrencesRepositoryInMemory,
 };
