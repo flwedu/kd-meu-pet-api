dc059aa87907327874d088c969865ee658c0c094
Eduardo Aquino <edutraquino@gmail.com>
2022-06-06 19:09:57 -0300

    test: + test for occurrencies and using test.each

--- src/domain/use-cases/create-use-case.spec.ts
+++ src/domain/use-cases/create-use-case.spec.ts
@@ -2,59 +2,57 @@ import User from "../entities/user";
 import IRepository from "../../output/repositories/repository-interface";
 import {
   AnimalsRepositoryInMemory,
+  OccurrencesRepositoryInMemory,
   UsersRepositoryInMemory,
 } from "../../output/repositories/in-memory";
 import makeCreateUseCaseFn from "./create-use-case";
 import {
   createFakeUser,
   createFakeAnimal,
+  createFakeOccurrence,
 } from "../../utils/fake-entity-factory";
 import Animal from "../entities/animal";
+import Occurrence from "../entities/occurrence";
 
-describe("# Create Use Case function to User Entity #", () => {
-  let repository: IRepository<User.Entity>;
-  const props = createFakeUser({}).props;
-
-  beforeEach(() => {
-    repository = new UsersRepositoryInMemory();
-  });
-
-  test("given valid props, should save a entity and return the Id", async () => {
-    const spy = jest.spyOn(repository, "save");
-    const sut = makeCreateUseCaseFn<User.Entity>(repository, "users");
-
-    const createdId = await sut(props);
-
-    expect.assertions(3);
-    expect(createdId).toEqual(expect.any(String));
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(await repository.findById(createdId)).toMatchObject({
-      id: createdId,
-      props,
+describe.each`
+  entity               | entityName       | createRepository                             | props
+  ${User.Entity}       | ${"users"}       | ${() => new UsersRepositoryInMemory()}       | ${createFakeUser({}).props}
+  ${Animal.Entity}     | ${"animals"}     | ${() => new AnimalsRepositoryInMemory()}     | ${createFakeAnimal({}).props}
+  ${Occurrence.Entity} | ${"occurrences"} | ${() => new OccurrencesRepositoryInMemory()} | ${createFakeOccurrence({}).props}
+`(
+  "# Create Use Case function to $entity #",
+  ({ entity, entityName, createRepository, props }) => {
+    beforeEach(() => {
+      jest.clearAllMocks();
     });
-  });
-});
 
-describe("# Create Use Case function to Animal Entity #", () => {
-  let repository: IRepository<Animal.Entity>;
-  const props = createFakeAnimal({}).props;
+    test("given valid props, should save a entity and return the Id", async () => {
+      const repository = createRepository();
+      const spy = jest.spyOn(repository, "save");
+      const sut = makeCreateUseCaseFn<typeof entity>(repository, entityName);
 
-  beforeEach(() => {
-    repository = new AnimalsRepositoryInMemory();
-  });
+      const createdId = await sut(props);
 
-  test("given valid props, should save a entity and return the Id", async () => {
-    const spy = jest.spyOn(repository, "save");
-    const sut = makeCreateUseCaseFn<Animal.Entity>(repository, "animals");
-
-    const createdId = await sut(props);
-
-    expect.assertions(3);
-    expect(createdId).toEqual(expect.any(String));
-    expect(spy).toHaveBeenCalledTimes(1);
-    expect(await repository.findById(createdId)).toMatchObject({
-      id: createdId,
-      props,
+      expect.assertions(3);
+      expect(createdId).toEqual(expect.any(String));
+      expect(spy).toHaveBeenCalledTimes(1);
+      expect(await repository.findById(createdId)).toMatchObject({
+        id: createdId,
+        props,
+      });
     });
-  });
-});
+    test.each(["animal", null, undefined, "xyz", ""])(
+      "Given a invalid entityName, should throw an Error",
+      async (invalidEntityName) => {
+        let repository: IRepository<typeof entity>;
+        try {
+          //@ts-expect-error
+          makeCreateUseCaseFn<Animal.Entity>(repository, invalidEntityName);
+        } catch (error) {
+          expect.assertions(1);
+          expect(error).toBeTruthy();
+        }
+      }
+    );
+  }
+);
