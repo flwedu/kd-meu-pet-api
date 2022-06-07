97209ff0f34b690e0df5a60cd22f4a400888a79d
Eduardo Aquino <edutraquino@gmail.com>
2022-06-06 23:15:50 -0300

    refact: remove async instruction

--- src/domain/use-cases/find-by-id-use-case.ts
+++ src/domain/use-cases/find-by-id-use-case.ts
@@ -1,5 +1,5 @@
 import IRepository from "../../output/repositories/repository-interface";
 
 export default function makeFindByIdUseCase<T>(repository: IRepository<T>) {
-  return async (id: string) => repository.findById(id);
+  return (id: string) => repository.findById(id);
 }
