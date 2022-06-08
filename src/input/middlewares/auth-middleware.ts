6a284c524cf4145415aef72e0ddfc2beeb29cc98
Eduardo Aquino <edutraquino@gmail.com>
2022-06-08 19:27:28 -0300

    feat: auth middleware factory

---
+++ src/input/middlewares/auth-middleware.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../../domain/entities/user";
+import { AuthenticationError } from "../../domain/errors/auth-error";
+import IRepository from "../../output/repositories/repository-interface";
+
+export function makeAuthenticationMiddleware(
+  repository: IRepository<User.Entity>,
+  roles: User.Role[]
+) {
+  function checkPrivileges(userRole: User.Role) {
+    return userRole in roles;
+  }
+
+  return async (
+    req: Request & { session: { loggedId: undefined | string } },
+    res: Response,
+    next: NextFunction
+  ) => {
+    const { loggedId } = req.session;
+    if (!loggedId) throw new AuthenticationError("Authentication is required");
+
+    try {
+      const loggedUser = await repository.findById(loggedId);
+      if (checkPrivileges(loggedUser.props.role)) {
+        next();
+      }
+      throw new AuthenticationError(
+        "Logged user has not privileges to do this operation"
+      );
+    } catch (error) {
+      next(error);
+    }
+  };
+}
