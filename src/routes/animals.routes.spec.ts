29c21b66d8007881de2a7b86368bf503f9154b6b
Eduardo Aquino <edutraquino@gmail.com>
2022-06-08 10:11:41 -0300

    test: refactoring and add #POST  tests

--- src/routes/animals.routes.spec.ts
+++ src/routes/animals.routes.spec.ts
@@ -3,22 +3,59 @@ import { configureExpress } from "../config/config-express-app";
 import { AnimalsRepositoryInMemory } from "../output/repositories/in-memory";
 import { createFakeAnimal } from "../utils/fake-entity-factory";
 
-describe("## load animals routes ##", () => {
+describe("# Animals routes #", () => {
   const repository = new AnimalsRepositoryInMemory();
   const app = configureExpress({ animals: repository });
 
-  describe("When #GET to api/animals/:id", () => {
-    test("for a valid id, should return 200 OK", async () => {
-      await repository.save(createFakeAnimal({}, "1").entity);
-      await supertest(app).get("/api/animals/1").expect(200);
-    });
+  beforeAll(async () => {
+    await repository.save(createFakeAnimal({}, "1").entity);
+  });
 
-    test("for a inexistent id, should return 404", async () => {
-      await supertest(app).get("/api/animals/2").expect(404);
+  describe("# GET", () => {
+    describe("api/animals/:id", () => {
+      test("with a valid id, should return 200 OK", async () => {
+        await supertest(app).get("/api/animals/1").expect(200);
+      });
+
+      test("with a inexistent id, should return 404", async () => {
+        await supertest(app).get("/api/animals/2").expect(404);
+      });
+    });
+    test("/api/animals, should return 404", async () => {
+      await supertest(app).get("/api/animals").expect(404);
     });
   });
 
-  test("When #GET to /api/animals, should return 404", async () => {
-    await supertest(app).get("/api/animals").expect(404);
+  describe("#POST to /api/animals", () => {
+    const animal = createFakeAnimal({});
+    test("with valid body data, should return 201", async () => {
+      const data = {
+        id: animal.entity.id,
+        ...animal.props,
+      };
+
+      const response = await supertest(app).post("/api/animals").send(data);
+
+      expect.assertions(2);
+      expect(response.statusCode).toEqual(201);
+      expect(response.body).toEqual({
+        message: `created successfully with id ${animal.entity.id}`,
+      });
+    });
+
+    test.each([{ name: null }, { description: null }])(
+      "with missing props, should return 400",
+      async (props) => {
+        const data = {
+          ...animal.props,
+          ...props,
+        };
+
+        const response = await supertest(app).post("/api/animals").send(data);
+
+        expect.assertions(1);
+        expect(response.statusCode).toEqual(400);
+      }
+    );
   });
 });
