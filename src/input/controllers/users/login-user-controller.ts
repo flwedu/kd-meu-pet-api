a6dfb3c9761ee99881658013deb8c0749319b320
Eduardo Aquino <edutraquino@gmail.com>
2022-06-13 21:20:40 -0300

    refactoring: returning error response

--- src/input/controllers/users/login-user-controller.ts
+++ src/input/controllers/users/login-user-controller.ts
@@ -3,6 +3,7 @@ import User from "../../../domain/entities/user";
 import { makeLoginUserUseCase } from "../../../domain/use-cases/user/login-user-use-case";
 import IRepository from "../../../output/repositories/repository-interface";
 import IEncryptor from "../../../security/encryptor-interface";
+import { createErrorResponse } from "../../response-factory/error-response-factory";
 import { createSuccessResponse } from "../../response-factory/success-response-factory";
 import { LoginSession } from "./session";
 
@@ -11,14 +12,18 @@ export function makeLoginUserController(
   encryptor: IEncryptor
 ) {
   return async (req: Request, res: Response, next: NextFunction) => {
-    const session = req.session as LoginSession;
-    const { username, password } = req.body;
-    const loginUseCase = makeLoginUserUseCase(repository, encryptor);
-    const user = await loginUseCase({ username, password });
+    try {
+      const session = req.session as LoginSession;
+      const { username, password } = req.body;
+      const loginUseCase = makeLoginUserUseCase(repository, encryptor);
+      const user = await loginUseCase({ username, password });
 
-    if (user) {
-      session.loggedId = user.id;
-      return createSuccessResponse(res).ok({ message: "Login successful" });
+      if (user) {
+        session.loggedId = user.id;
+        return createSuccessResponse(res).ok({ message: "Login successful" });
+      }
+    } catch (error) {
+      return createErrorResponse(error, res);
     }
   };
 }
