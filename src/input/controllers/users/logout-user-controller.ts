3e6e3c493b8716f995f0146436c8f064daec8501
Eduardo Aquino <edutraquino@gmail.com>
2022-06-14 21:15:18 -0300

    refact: controller logic

--- src/input/controllers/users/logout-user-controller.ts
+++ src/input/controllers/users/logout-user-controller.ts
@@ -5,12 +5,10 @@ import { LoginSession } from "./session";
 
 export function makeLogoutController() {
   return async (req: Request, res: Response) => {
-    try {
-      const session = req.session as LoginSession;
-      session.destroy();
+    const session = req.session as LoginSession;
+    session.destroy((err: any) => {
+      if (err) return createErrorResponse(err, res);
       return createSuccessResponse(res).ok({ message: "Logout successful" });
-    } catch (error) {
-      return createErrorResponse(error, res);
-    }
+    });
   };
 }
