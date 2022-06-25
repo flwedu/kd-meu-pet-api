ad5462e9da00ed16e311c4bbac31ccf60688b19f
Eduardo Aquino <edutraquino@gmail.com>
2022-06-25 15:03:51 -0300

    refact: exporting arrow function

--- src/input/middlewares/catch-error.ts
+++ src/input/middlewares/catch-error.ts
@@ -1,16 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import { createErrorResponse } from "../response-factory/error-response-factory";
 
-export function catchError(
+export const catchError = (
   error: Error,
   _: Request,
   res: Response,
   next: NextFunction
-) {
+) => {
   if (error) {
     const errorResponse = createErrorResponse(error, res);
     return errorResponse();
   }
 
   return next();
-}
+};
