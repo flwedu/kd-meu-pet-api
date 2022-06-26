733ce104ac694c89840feef4dd7d41c3956825ee
Eduardo Aquino <edutraquino@gmail.com>
2022-06-26 12:14:17 -0300

    feat: get entity name from request.path or baseUrl

--- src/input/controllers-factory/register-controller.ts
+++ src/input/controllers-factory/register-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { OperationError } from "../../domain/errors/operation-error";
 import { RegisterUseCase } from "../../domain/use-cases";
 import IRepository from "../../output/repositories/repository-interface";
 import IEncryptor from "../../security/encryptor-interface";
@@ -12,8 +13,9 @@ export class RegisterController<T> implements IController {
     private readonly encryptor: IEncryptor
   ) {}
 
-  async handle(req: Partial<Request>, res: Response, next: any) {
-    const entityName = req.path;
+  async handle(req: Request, res: Response, next: any) {
+    const path = req.path.length > 1 ? req.path : req.baseUrl;
+    const entityName = extractEntityNameFromPath(path);
     const props = req.body;
 
     const useCase = new RegisterUseCase<T>(
@@ -30,4 +32,15 @@ export class RegisterController<T> implements IController {
       next(error);
     }
   }
+}
+
+function extractEntityNameFromPath(path: string) {
+  const findPathRegex = /api\/(\w+)\/?/gi;
+  const matchResults = findPathRegex.exec(path);
+  if (!matchResults) {
+    throw new OperationError(
+      `Could not extract entity name from path: ${path}`
+    );
+  }
+  return matchResults[1];
 }
