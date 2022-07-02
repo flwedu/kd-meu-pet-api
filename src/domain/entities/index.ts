1347b30354839ee4ed273815bc167c29f26f025c
Eduardo Aquino <edutraquino@gmail.com>
2022-07-02 11:23:16 -0300

    feat: add types

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -2,4 +2,18 @@ import User from "./user";
 import Animal from "./animal";
 import Occurrence from "./occurrence";
 
-export { User, Animal, Occurrence };
+type EntityTypes = {
+  users: User.Entity;
+  animals: Animal.Entity;
+  ocurrences: Occurrence.Entity;
+};
+
+type EntityProps = {
+  users: User.Props;
+  animals: Animal.Props;
+  ocurrences: Occurrence.Props;
+};
+
+type EntityNames = keyof EntityTypes;
+
+export { User, Animal, Occurrence, EntityTypes, EntityProps, EntityNames };
